9d839c6a49b3f24b11e3f471c98051125d446db4
abdallah.selim@bld.ai <abdallah.selim@bld.ai>
2024-05-03 18:20:16 +0300

    adding test requests so we can check the capacity of the buffer

---
+++ README.md
@@ -0,0 +1,7 @@
+to run the server use this format :
+`./wserver -d <rootdirectory> -p <port number> -t <number of threads>
+`
+example :
+`./wserver -d . -p 8004 -t 5
+`
+
---
+++ wclient_req_test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Number of requests to send
+NUM_REQUESTS=10
+
+# Server details
+SERVER_HOST="localhost"
+SERVER_PORT="8004"
+REQUEST_PATH="/spin.cgi"
+
+# Send multiple requests concurrently
+for ((i=0; i<$NUM_REQUESTS; i++)); do
+    ./wclient $SERVER_HOST $SERVER_PORT $REQUEST_PATH &
+done
+
+# Wait for all requests to finish
+wait
